8e7c53674ade6205ae782622099b3a14482ce9db
fsaki13@gmail.com <fsaki13@gmail.com>
2022-10-23 23:14:23 +0200

    korekcije u migracijama za dropovanje spoljnog kljuca

--- database/migrations/2022_10_22_171735_add_user_id_to_registrations_table.php
+++ database/migrations/2022_10_22_171735_add_user_id_to_registrations_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('registrations', function (Blueprint $table) {
-            $table->dropColumn('user_id');
+            $table->dropForeign('user_id');
         });
     }
 };
--- database/migrations/2022_10_22_171956_add_tournament_id_to_registrations_table.php
+++ database/migrations/2022_10_22_171956_add_tournament_id_to_registrations_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('registrations', function (Blueprint $table) {
-            $table->dropColumn('tournament_id');
+            $table->dropForeign('tournament_id');
         });
     }
 };
