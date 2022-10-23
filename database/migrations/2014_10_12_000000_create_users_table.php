42490b6a6163d431fd0717b2ed1dd1d6eb0dc045
fsaki13@gmail.com <fsaki13@gmail.com>
2022-10-23 23:15:20 +0200

    dodato par kolona za User table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
+            $table->string('country');
+            $table->string('city');
+            $table->string('club');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
