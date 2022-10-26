7705a3467c4a5126e517530fe4997cc22a8456e2
fsaki13@gmail.com <fsaki13@gmail.com>
2022-10-26 21:59:40 +0200

    napravljene funkcije store, update i destroy za registracije

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\RegistrationResource;
 use App\Models\Registration;
 use Illuminate\Contracts\Routing\Registrar;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RegistrationController extends Controller
 {
@@ -30,5 +31,71 @@ class RegistrationController extends Controller
     public function show(Registration $registration)
     {
         return new RegistrationResource($registration);
+    }
+
+    public function store(Request $request)
+    {
+        //post request - cuvamo objekat u bazi
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required|string',
+            'surname'=> 'required|string',
+            'category'=> 'required|string',
+            'belt'=> 'required|string',
+            'event_name'=> 'required|string',
+            'user_id'=> 'required|string',
+            'tournament_id'=> 'required|string',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $registration = Registration::create([
+            'name'=> $request->name,
+            'surname'=> $request->surname,
+            'category'=> $request->category,
+            'belt'=> $request->belt,
+            'event_name'=> $request->event_name,
+            'user_id'=> $request->user_id,
+            'tournament_id'=> $request->tournament_id
+        ]);
+
+        return response()->json(['Successfully registered.', new RegistrationResource($registration)]);
+    }
+
+    public function update(Request $request, Registration $registration)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required|string',
+            'surname'=> 'required|string',
+            'category'=> 'required|string',
+            'belt'=> 'required|string',
+            'event_name'=> 'required|string',
+            'user_id'=> 'required|string',
+            'tournament_id'=> 'required|string',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $registration->name = $request->name;
+        $registration->surname = $request->surname;
+        $registration->category = $request->category;
+        $registration->belt = $request->belt;
+        $registration->event_name = $request->event_name;
+        $registration->user_id = $request->user_id;
+        $registration->tournament_id = $request->tournament_id;
+
+        $registration->save();
+
+        return response()->json(['Registration is updated successfully',new RegistrationResource($registration)]);
+    }
+
+    public function destroy(Registration $registration)
+    {
+        $registration->delete();
+
+        return response()->json('Registration is successfully deleted.');
     }
 }
